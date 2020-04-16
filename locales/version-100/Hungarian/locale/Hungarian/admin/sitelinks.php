f5abf84bfac8d2c6963db52752d7b7faad7ba889
karrak1 <g.sebestyen64@gmail.com>
2020-04-16 19:32:50 +0200

    Update sitelinks.php

--- locales/version-100/Hungarian/locale/Hungarian/admin/sitelinks.php
+++ locales/version-100/Hungarian/locale/Hungarian/admin/sitelinks.php
@@ -38,7 +38,7 @@ $locale["SL_0042"] = "Állítsd be a webhelyed Link Navigációs beállításait
 $locale["SL_0043"] = "Link Oldalanként";
 $locale["SL_0044"] = "Maximálisan látható Linkek a Navigációs sávban";
 $locale["SL_0045"] = "Linkek száma";
-$locale["SL_0046"] = "Link csoportosítása";
+$locale["SL_0046"] = "";
 $locale["SL_0047"] = "Link csoportok amikor a Linkek meghaladják a Linkek oldalanként";
 $locale["SL_0048"] = "Nem, ne rakja csoportba a Linkeket";
 $locale["SL_0049"] = "Igen, rakja csoportba a Linkeket \"Több megjelenítése\"";
@@ -50,7 +50,7 @@ $locale["SL_0052"] = "Sorrend";
 $locale["SL_0053"] = "Lehetőségek";
 
 $locale["SL_0062"] = "Nincs létrehozva Navigációs link.";
-$locale["SL_0063"] = "Használjon BBKódot a Link nevében?";
+$locale["SL_0063"] = "";
 
 $locale["SL_0070"] = "Ikon";
 $locale["SL_0071"] = "Új ablak?";
@@ -65,3 +65,42 @@ $locale["SL_0080"] = "Biztosan törölni szeretnéd ezt a Linket?";
 $locale["SL_0085"] = "Add meg a Link nevét";
 $locale["SL_0086"] = "Add meg a Link URL-jét";
 $locale["SL_0087"] = "Nincs Navigációs Link. Kérjük, ellenőrizze a Navigációs Linket.";
+
+// New Sitelinks Administration Locale
+
+$locale["SL_0100"] = "Navigációs Link Kezelő";
+$locale["SL_0101"] = "Frissítési hiba";
+$locale["SL_0102"] = "Navigációs Link";
+$locale["SL_0103"] = "A linkek sikeresen hozzáadódtak.";
+$locale["SL_0104"] = "Valami baj történt a menü létrehozásával.";
+$locale["SL_0105"] = "A linkek sikeresen frissülnek.";
+$locale["SL_0106"] = "A link sikeresen eltávolításra került.";
+$locale["SL_0107"] = "A menü frissítése sikeres volt.";
+$locale["SL_0108"] = "Ez a művelet eltávolítja a menüt és a menüben található összes linket. Folytatja?";
+
+$locale["SL_0200"] = "A menü eltávolítva.";
+$locale["SL_0201"] = "Új menü került hozzáadásra.";
+$locale["SL_0202"] = "A menüazonosító nincs meghatározva.";
+
+$locale["SL_0300"] = "Válassza ki a szerkeszteni kívánt menüt";
+$locale["SL_0301"] = "Választ";
+$locale["SL_0302"] = "Új menű létrehozása";
+$locale["SL_0303"] = "Egyéni linkek";
+$locale["SL_0304"] = "Link Neve";
+$locale["SL_0305"] = "Link URL";
+$locale["SL_0306"] = "Hozzáadás az aktuális menühöz";
+$locale["SL_0307"] = "Menü neve";
+$locale["SL_0308"] = "Cím attribútuma";
+$locale["SL_0309"] = "Link leírása";
+$locale["SL_0310"] = "Link ikon osztály";
+$locale["SL_0311"] = "Nyissa meg a linket egy új lapon";
+$locale["SL_0312"] = "Link Hozzáférés";
+$locale["SL_0313"] = "Link Státusz";
+$locale["SL_0314"] = "Eltávolít";
+$locale["SL_0315"] = "Link Mentése";
+$locale["SL_0316"] = "Használjon BBCode-t a Link Nevében?";
+$locale["SL_0317"] = "Link Csoport";
+$locale["SL_0318"] = "Ne csoportosítsa a linkeket";
+$locale["SL_0319"] = "Linkek csoportosítása \"Többi megjelenítése\"";
+$locale["SL_0320"] = "Linkek Oldalanként";
+$locale["SL_0321"] = "Menű Mentése";
