1e0bc126e7eecc2eef8f860b2b34c6be7e4fa587
karrak1 <g.sebestyen64@gmail.com>
2020-06-13 08:00:09 +0200

    Update quote.php

--- locales/version-800/Hungarian/locale/Hungarian/bbcodes/quote.php
+++ locales/version-800/Hungarian/locale/Hungarian/bbcodes/quote.php
@@ -1,4 +1,7 @@
 <?php
 $locale['bb_quote_description'] = "Szöveg idézése";
 $locale['bb_quote_usage'] = "Idézett szöveg";
-
+$locale['bb_quote_expand'] = "Teljes szöveg nézése";
+$locale['bb_quote_collapse'] = "Teljes szöveg rejtése";
+$locale['bb_quote'] = "Idézet";
+$locale['bb_wrote'] = "írt";
